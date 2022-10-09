22d6155eea90dd69fd482a0d5833a0e67fd08f57
AriannaRamoutar <77646032+AriannaRamoutar@users.noreply.github.com>
2022-10-09 19:57:36 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,31 @@
+Click==7.0
+Flask==1.0.2
+Flask-Cors==3.0.7
+Flask-JWT==0.3.2
+Flask-SQLAlchemy==2.3.2
+gunicorn==19.9.0
+itsdangerous==1.1.0
+Jinja2>=2.10.1
+MarkupSafe==1.1.1
+PyJWT==1.4.2
+six==1.12.0
+SQLAlchemy==1.3.1
+werkzeug>=0.15.3
+#installing dependencies from requirements.txt
+
+  sudo apt-get install -y python3.6 python3-pip python3.6-dev 
+  #command to install python and pip3
+  
+  sudo apt-get install git-all
+  #command to install Git
+  
+    echo "$requirements" > requirements.txt
+    #used to create a requirements file
+    
+    pip3 install -r requirements
+    #for the user to install all the dependencies from the requirements file
+    
+gunicorn.wsgi.app
+#server should start after executing this command 
+  
+  
